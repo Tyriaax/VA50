4558efd0dda85cd5a5ddd50c45e905dec0795512
aet <aetesse31@gmail.com>
2021-11-02 21:32:14 +0100

    pieces_recognition

--- ObjectDetection/game_board_recognition.py
+++ ObjectDetection/game_board_recognition.py
@@ -11,6 +11,45 @@ def get_object_position():
 def get_pieces_orientation():
   pass
 
+def detect_pieces_through_color(frame, image_infos):
+  """lower = np.array([15,150,20]) #yellow askip
+  upper = np.array([35,255,255])
+
+  lower = np.array([161,155,84]) #red askip
+  upper = np.array([179,255,255])"""
+
+  #lower = np.array([94,80,2]) #blue askip
+  #upper = np.array([126,255,255])
+
+  #lower = np.array([0,0,128]) #white askip
+  #upper = np.array([255,255,255])
+
+  colors_detected = [
+    [np.array([94,80,2]), np.array([126,255,255]), "Sherlock" ], #blue
+    #[np.array([0,0,128]), np.array([255,255,255]), "Ogre"] #white
+  ]
+
+  image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+  for color in colors_detected:
+    #mask = cv2.inRange(image, lower, upper)
+    mask = cv2.inRange(image, color[0], color[1])
+    #blue = cv2.bitwise_and(frame, frame, mask = mask) Affichage du mask
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) != 0:
+      for contour in contours:
+        if cv2.contourArea(contour) > 500:
+          x, y, w, h = cv2.boundingRect(contour)
+          isRecognised = sift_detection(frame[y: y + h, x : x + w], image_infos)
+          if isRecognised:
+            cv2.rectangle(frame, (x,y), (x + w, y + h), (0,0,255), 3)
+            cv2.putText(frame, color[2], (x,y),1,1,(0,0,255),3)
+
+
+  return frame
+
 def get_homographied_board(img, pts_src):
 
   #im_src = cv2.imread(os.path.abspath(os.path.join(os.path.dirname( __file__ ), "plateau.jpg")))
@@ -40,7 +79,7 @@ def get_keypoints(images):
   return list_image_info
 
 def sift_detection(current_img, images_infos : list):
-  MIN_MATCHES = 55
+  MIN_MATCHES = 20
 
   for image_info in images_infos:
     
@@ -59,10 +98,10 @@ def sift_detection(current_img, images_infos : list):
 
     if(len(matches) >= MIN_MATCHES):
       #img3 = draw_boxes(matches,keypoints_1,img1,keypoints_2,img2)
-      matchedImg = cv2.drawMatches(img, keypoints_1, image_info[0], image_info[1], matches[:30], image_info[0], flags=2)
-      return matchedImg
+      #matchedImg = cv2.drawMatches(img, keypoints_1, image_info[0], #image_info[1], matches[:30], image_info[0], flags=2)
+      return True
 
-  return current_img
+  return False
 
 def load_kp_samples(PATH_SAMPLES):
 
@@ -82,6 +121,7 @@ def load_kp_samples(PATH_SAMPLES):
     list_image_info.append(image_info)
 
   return list_image_info
+
 
 list_board_coords = []
 def mousePoints(event,x,y,flags,params):
@@ -115,6 +155,8 @@ def video_recognition():
         cv2.circle(img,coord,10,(0,255,0),-1)
     else:  
       img = get_homographied_board(img, np.array(list_board_coords))
+      img = detect_pieces_through_color(img, images_infos)
+      #cv2.imshow("mask", mask)
 
     cv2.imshow(window_name, img)
 
