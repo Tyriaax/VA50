472f95774f3d43890cfb5813d6fcf694b56bc32a
Malek FARDEAU-SPIRE <38687753+M003T@users.noreply.github.com>
2021-11-15 13:55:41 +0100

    nothing special

--- ObjectDetection/main.py
+++ ObjectDetection/main.py
@@ -4,10 +4,13 @@ from video_recognition import *
 from image_recognition import *
 
 def main():
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'zebi.mp4'))
-    img = cv2.imread(path)
-    video_recognition()
-    #video_recognition(path)
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'video_test.mp4'))
+    imagepath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'plateau.jpg'))
+
+    #image_recognition(imagepath)
+    #video_recognition()
+    video_recognition(path)
+
 
 if __name__ == '__main__':
     main()
