a8dd2ec7b030eefbeed6fd5903c8e7c37c52d3b7
Malek FARDEAU-SPIRE <38687753+M003T@users.noreply.github.com>
2022-01-01 01:42:59 +0100

    Verifies device

--- ObjectDetection/CNN/CNNLearning.py
+++ ObjectDetection/CNN/CNNLearning.py
@@ -160,6 +160,8 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    print(device)
+
     # Affichage de quelques images
     inputs, classes = next(iter(dataloaders['train']))
     out = torchvision.utils.make_grid(inputs)
